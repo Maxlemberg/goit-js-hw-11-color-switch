63674c56570a3d1ea391d9a7b8ffe415dbbf6171
Maksym <maksymuhf@gmail.com>
2021-02-06 15:00:29 +0200

    switch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-import './styles.css';
+import colors from './js/library.js';
+import colorsArr from './js/library.js';
+import fnRandom from './js/randomizer.js';
+
+const startBtn = document.querySelector('[data-action=start]');
+const stopBtn = document.querySelector('[data-action=stop]');
+const bodyRef = document.querySelector('body');
+let setColor = null;
+
+const handleTheme = function (e) {
+  startBtn.removeEventListener('click', handleTheme);
+  console.log('Start');
+  setColor = setInterval(() => {
+    bodyRef.style.backgroundColor = colorsArr[fnRandom(0, colorsArr.length)];
+  }, 1000);
+};
+startBtn.addEventListener('click', handleTheme);
+
+const handleStopTheme = function () {
+  console.log('Stop');
+  clearInterval(setColor);
+  startBtn.addEventListener('click', handleTheme);
+};
+stopBtn.addEventListener('click', handleStopTheme);
